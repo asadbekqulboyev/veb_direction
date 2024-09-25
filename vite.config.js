2ed9e050330e8ff5c3d6bd65e9b82538815aaef8
asadbek <asadbekqulboyev@gmail.com>
2024-09-25 21:40:24 +0500

    main

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
+      // jQuery tashqi modul sifatida ko'rsatilmoqda
       external: ['jquery'],
-    },
-  },
+      output: {
+        globals: {
+          jquery: '$'
+        }
+      }
+    }
+  }
 });
